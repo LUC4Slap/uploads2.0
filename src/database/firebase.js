6e6ed56332591576857de41fe2a9555f4a9faa59
LUC4Slap <lucaslap27@gmail.com>
2021-06-04 13:30:17 -0400

    Ajusta conf firebase

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,5 +1,13 @@
 import firebase from "firebase";
-let firebaseConfig = {};
+let firebaseConfig = {
+  apiKey: "AIzaSyBrYg9mkMxWSUYCald3yIK_-XbKOs8AIzg",
+  authDomain: "uploads-a728e.firebaseapp.com",
+  projectId: "uploads-a728e",
+  storageBucket: "uploads-a728e.appspot.com",
+  messagingSenderId: "882038596193",
+  appId: "1:882038596193:web:70c8eafab2d96145cc8020",
+  measurementId: "G-ST5JFPBDS9",
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
